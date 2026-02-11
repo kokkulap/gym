c1dafb304f5b7c67a677d3fea75dcaf512818aea
kokkulap <theultimategym2.0@gmail.com>
2026-02-11 22:54:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 // --- FIREBASE CONFIGURATION ---
 // We use 'window.' to make these keys visible to the rest of the app
 window.firebaseConfig = {
-    apiKey: "AIzaSyDaR4pZPQ7fD68KHmjvH1oxwgseW83ncvA",
-    authDomain: "the-ultimate-gym-2.firebaseapp.com",
-    projectId: "the-ultimate-gym-2",
-    storageBucket: "the-ultimate-gym-2.firebasestorage.app",
-    messagingSenderId: "836314437510",
-    appId: "1:836314437510:web:aa0066307393e250b6be07"
+    apiKey: "AIzaSyCbhY-fV64dDSH9dYRbwWR2cmOyEPWa5jM",
+    authDomain: "kokkulagym.firebaseapp.com",
+    projectId: "kokkulagym",
+    storageBucket: "kokkulagym.firebasestorage.app",
+    messagingSenderId: "436253669857",
+    appId: "1:436253669857:web:9015ffbcf7d60bc35ebaa0"
+
 };
